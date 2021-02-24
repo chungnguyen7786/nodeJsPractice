0b5be9c471f6a6c17aebbb758e370fbf8a78642c
Glitch (lofty-pushy-sea) <none>
2021-02-24 16:05:40 +0000

    Unit 2

--- server.js
+++ server.js
@@ -3,18 +3,20 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './');
+
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
 });
 
-app.get('/todos', (req, res) => {
-  res.send('<ul><li>Đi chợ</li><li>Nấu cơm</li><li>Rửa bát</li><li>Học code tại Coders-X</li></ul>');
-})
-
+app.get("/todos", (req, res) => {
+  res.render("index.pug");
+});
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
