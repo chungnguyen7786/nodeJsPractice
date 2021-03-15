3b5e8201702d79eefaa1e8150440d59c36d1142a
Glitch (alder-general-walrus) <none>
2021-03-15 18:02:54 +0000

    Update users module for bookstore

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var db = require('./db.js');
 
 var books = db.get('books').value();
 
+var users = db.get('users').value();
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -16,6 +18,11 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 app.get("/", (req, res) => {
   res.render("index.pug");
 });
+
+
+
+
+//code for books
 
 app.get('/books', (req, res) => {
   var q = req.query.q;
@@ -45,6 +52,66 @@ app.post('/books/add', (req, res) => {
     db.get('books').push(req.body).write();
     res.redirect('/books');
 });
+
+
+
+
+// code for users
+app.get('/users', (req, res) => {
+  var q = req.query.q;
+  var matchedUsers;    
+  if (q === undefined) {
+    matchedUsers = users;
+  } else {
+    matchedUsers = users.filter((user) => {
+      return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+  }  
+  res.render("users.pug", {users: matchedUsers});
+});
+
+app.get('/users/:id/delete', (req, res) => {   
+    var id = req.params.id;
+    db.get('users').remove({id: id}).write();
+    res.redirect('/users');
+});
+
+app.get("/users/add", (req, res) => {
+  res.render("addUser.pug");
+});
+
+app.post('/users/add', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+});
+
+app.get('/users/:id/view', (req, res) => {   
+    var id = req.params.id;
+    var user = db.get('users').find({id: id}).value();
+    if (user) {
+        res.render('./viewUser.pug', {user: user});
+    }    
+});
+
+app.get("/users/:id/edit", (req, res) => {
+  var id = req.params.id;
+    var user = db.get('users').find({id: id}).value();
+    if (user) {
+        res.render('./editUser.pug', {user: user});
+    }
+});
+
+app.post('/users/:id/edit', (req, res) => {
+  var id = req.params.id;
+  db.get('users')
+    .find({id: id})
+    .assign({name: req.body.name, phone: req.body.phone })
+    .write();
+    res.redirect('/users');
+});
+
+
 
 // listen for requests :)
 app.listen(process.env.PORT, () => {
