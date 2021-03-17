6616fed0b62b39d7e6a5bfe72224b499becae64d
Glitch (truthful-cuboid-scapula) <none>
2021-03-17 05:49:30 +0000

    add validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,6 +29,25 @@ module.exports.create = (req, res) => {
 
 module.exports.postCreate = (req, res) => {
     req.body.id = shortid.generate();
+    var errors = [];
+    if(!req.body.name) {
+      errors.push('Name is required.');
+    } else if (req.body.name.length > 30) {      
+      errors.push("Name's length must be smaller than or equal to 30.");
+    }
+    
+    if(!req.body.phone) {
+      errors.push('Phone is required.');
+    }
+    
+    if(errors.length) {
+      res.render('users/addUser.pug', {
+        errors: errors,
+        values: req.body
+      });
+      return;
+     }
+  
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
